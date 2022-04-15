131ebed494b1a2a0de34bb8854dc28713e9ac0aa
Stephan <skruglov.2023@carmelhs.org>
2022-04-15 15:31:14 -0500

    Changed to Houndify from google

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import speech_recognition as sr
-filename = "lakeilfire-4-1637980535.wav"
+filename = "1650051437540.wav"
+ID = 'id'
+key = 'key'
+with open(ID, 'r') as guil:
+    global client_id
+    almostGuild = guil.read()
+    client_id = almostGuild
+with open(key, 'r') as toke:
+    global client_key
+    almostToken = toke.read()
+    client_key = almostToken
 # initialize the recognizer
 r = sr.Recognizer()
 # open the file
@@ -7,5 +17,5 @@ with sr.AudioFile(filename) as source:
     # listen for the data (load audio to memory)
     audio_data = r.record(source)
     # recognize (convert from speech to text)
-    text = r.recognize_google(audio_data)
+    text = r.recognize_houndify(audio_data, client_id, client_key)
     print(text)
